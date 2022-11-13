4c83fbbeb4d70afc97bef5c0efcbd67db3759f68
Priyajit Mukherjee <priyajit.mukherjee1@gmail.com>
2022-11-13 13:51:10 +0530

    changed port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const passportAuth = require('./config/passportConfig')
 const session = require('express-session')
 const passport = require('passport')
 
-const port = 5000
+const port = process.env.PORT || 5000
 const app = express()
 connectDatabase()
 passportAuth()
